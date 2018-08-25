f5c77d3587f0bc4f395d115958a4e7280bf60ddd
Zbigglord <Zbigglord@googlemail.com>
2018-08-25 17:00:53 +0200

    Update readme.php

--- readme.php
+++ readme.php
@@ -6,32 +6,31 @@ Needs Frontend code: -->
 <?php $slider = Hellux_Slider_Model_Slider::getSliderData(1); //1 is slider id so we may have as meny we want ?>
     <div id="slider">
         <div id="helluxSlider" class="carousel slide" data-ride="carousel">
-		<!-- maybe we want indicators (if not just comment it out)-->
+		<!-- maybe we want indicators (if not we may want just comment it out)-->
 		<ol class="carousel-indicators">
 		  <?php $i = 0;?>
           <?php foreach($slider as $item): ?>
-		    <li class="<?php if($i == 0):?>active<?php endif;?>" data-slide-to="<?=$i?>" data-target="#helluxSlider"><?php echo $i+1; ?></li><!-- comments remove spaces between inline-block elements -->
+		    <li class="<?php if($i == 0):?>active<?php endif;?>" data-slide-to="<?=$i?>" data-target="#helluxSlider"><?= $i+1; ?></li><!-- comments remove spaces between inline-block elements -->
 		   <?php $i++;?>
            <?php endforeach; ?>
 		</ol>		
-		
-		<!-- END maybe we want indicators -->
+              <!-- END maybe we want indicators -->
             <!-- Wrapper for slides -->
             <div class="carousel-inner" role="listbox">
                 <?php $i = 0;?>
                 <?php foreach($slider as $item): ?>
                 <div class="item <?php if($i == 0):?>active<?php endif;?>">
-                <img class="img-reponsive carousel-image" src="<?php echo $item['item_address'].''.$item['item_file_name'] ?>" />
+                <img class="img-reponsive carousel-image" src="<?= $item['item_address'].''.$item['item_file_name'] ?>" />
 				<div class="slider-caption-container">
 				 <div class="carousel-caption">
 				 <?php if($item['has_header'] == 1): ?>
 				 <?php if($item['has_link'] == 1): ?>
 				 <?php if($item['has_button'] != 1):?>
-				 <a class="slider-button-link" href="<?=$item['item_link'] ?>"><h3 class="slider-header"><?= $item['item_header_text'];?></h3></a>				 
+				 <a class="slider-button-link" href="<?= $item['item_link'] ?>"><h3 class="slider-header"><?= $item['item_header_text'];?></h3></a>				 
 				 <?php else: ?><!-- IF HAS BUTTON -->
 				 <h3 class="slider-header"><?= $item['item_header_text'];?></h3>
 				 <p class="slider-button-p">
-                    <a class="btn btn-lg btn-primary slider-button" role="button" href="http://<?php echo $item['item_link'] ?>" target="_blank"><?=$item['button_text'] ?></a>
+                    <a class="btn btn-lg btn-primary slider-button" role="button" href="http://<?= $item['item_link'] ?>" target="_blank"><?= $item['button_text'] ?></a>
                  </p>
 				 <?php endif; ?><!-- END IF HAS BUTTON -->
 				 
